874bb4383b5c968406dbc0bb98ec17dd4c53fbd1
karsten feliciano <115325687+Karsten182022@users.noreply.github.com>
2022-11-11 17:53:55 -0300

    Add files via upload

--- loja.js
+++ loja.js
@@ -1,6 +1,6 @@
-async function carregaritens() {
+async function carregarlivros() {
     console.log
-debugger
+    debugger
     var endpoint = "https://codifica-demo-api.herokuapp.com/api/v2/diariodeumbanana/products/category/livro"
 
     var resposta = await fetch(endpoint);
@@ -17,10 +17,63 @@ debugger
         resultado.append(img)
     }
 }
-function setConfig(){
-	var produtos = {
-		"produto": ""
-	};
-	document.title = texts.title;
-	document.getElementById("").innerHTML = produtos.produto;
+
+async function carregarcamisetas() {
+    console.log
+    debugger
+    var endpoint = "https://codifica-demo-api.herokuapp.com/api/v2/diariodeumbanana/products/category/camiseta"
+
+    var resposta = await fetch(endpoint);
+
+    var arquivojson = await resposta.json();
+
+    var resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = ""
+
+    for (var categoria of arquivojson) {
+        var img = document.createElement("img")
+        img.src = categoria.img
+        resultado.append(img)
+    }
+}
+
+async function carregarfilmes() {
+    console.log
+    debugger
+    var endpoint = "https://codifica-demo-api.herokuapp.com/api/v2/diariodeumbanana/products/category/filme"
+
+    var resposta = await fetch(endpoint);
+
+    var arquivojson = await resposta.json();
+
+    var resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = ""
+
+    for (var categoria of arquivojson) {
+        var img = document.createElement("img")
+        img.src = categoria.img
+        resultado.append(img)
+    }
+}
+
+async function carregarextras() {
+    console.log
+    debugger
+    var endpoint = "https://codifica-demo-api.herokuapp.com/api/v2/diariodeumbanana/products/category/extra"
+
+    var resposta = await fetch(endpoint);
+
+    var arquivojson = await resposta.json();
+
+    var resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = ""
+
+    for (var categoria of arquivojson) {
+        var img = document.createElement("img")
+        img.src = categoria.img
+        resultado.append(img)
+    }
 }
